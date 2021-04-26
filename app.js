78af77031bbbeb5a102d699d5a4a0ed19c05749d
NaurisVersis <nauris.versis@gmail.com>
2021-04-26 16:15:21 +0300

    fixed css problems

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
-
-
+const path = require("path");
 
 const app = express();
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "/")));
 
 app.get("/", function (req,res) {
-  res.sendFile(__dirname + "/index.html")
+  res.sendFile(__dirname + "/index.html");
+  console.log(__dirname);
 })
 
 
